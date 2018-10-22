f1c1c59fd3de9a7cfbdd825eeaabcaa405dfb8ec
jmrenouard <Ght9vtts>
2018-10-22 11:28:24 +0200

    Exemple de gestion des arguments

--- exercice2/affiche_args.py
+++ exercice2/affiche_args.py
@@ -2,7 +2,8 @@
 import sys
 
 def affiche_args(*args):
+    print("Il y a %d argument(s)" % len(*args))
     for i, arg in enumerate(*args):
-        print("* %d) %s" % (i, arg))
+        print("* %d) %s" % (i+1e, arg))
 if __name__ == '__main__':
     affiche_args(sys.argv)
