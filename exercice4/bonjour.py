a8e4ed161ee13ad90629d9e87e57649bc6116c0d
jmrenouard <Ght9vtts>
2018-10-24 11:56:13 +0200

    Adding module samples

---
+++ exercice4/bonjour.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def dit_bonjour():
+    print('Bonjour !')
+
+
+def dit_hello():
+    print('Hello !')
+
+
+def print_kwargs(**kwargs):
+    print(kwargs)
+
+
+if __name__ == "main":
+    dit_bonjour()
